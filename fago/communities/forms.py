64ee3a786b05ea827f2653be0e07679022406cf5
Rachit74 <rachithooda09@gmail.com>
2024-08-24 11:00:52 +0530

    Restarting a new project

--- fago/communities/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import Community
-
-# Create Commnity Form
-class CreateCommunityForm(forms.ModelForm):
-
-    class Meta:
-        model = Community
-        fields = ['name', 'description']
