02f6953100f6d7fadf40732b450f8333e75a3087
rachit74 <rachithooda09@gmail.com>
2024-09-13 22:08:49 +0530

    Add: single post api endpoint

--- fago/api/urls.py
+++ fago/api/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('posts/', views.get_posts, name='get_posts_endpoint'),
+    path('posts/<int:id>/', views.get_post, name='get_post_endpoint'),
 ]
--- fago/api/views.py
+++ fago/api/views.py
@@ -11,3 +11,10 @@ def get_posts(request):
     posts = Post.objects.all()
     serializer = PostSerializer(posts, many=True)
     return Response(serializer.data)
+
+# Get one particular post
+@api_view(['GET'])
+def get_post(request, id:int):
+    post = Post.objects.get(id=id)
+    serializer = PostSerializer(post)
+    return Response(serializer.data)
